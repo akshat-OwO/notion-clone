8c6887cee923375e548b8801ed6492d5cfd6c2e4
Akshat <akshatsinghdelhi@gmail.com>
2023-12-03 18:28:21 +0530

    trash box

--- app/(main)/_components/Navigation.tsx
+++ app/(main)/_components/Navigation.tsx
@@ -1,15 +1,29 @@
 "use client";
 
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover";
 import { api } from "@/convex/_generated/api";
 import { cn } from "@/lib/utils";
 import { useMutation } from "convex/react";
-import { ChevronsLeft, MenuIcon, PlusCircle, Search, Settings } from "lucide-react";
+import {
+    ChevronsLeft,
+    MenuIcon,
+    Plus,
+    PlusCircle,
+    Search,
+    Settings,
+    Trash,
+} from "lucide-react";
 import { usePathname } from "next/navigation";
 import React, { ElementRef, FC, useEffect, useRef, useState } from "react";
 import { toast } from "sonner";
 import { useMediaQuery } from "usehooks-ts";
 import DocumentList from "./DocumentList";
 import Item from "./Item";
+import TrashBox from "./TrashBox";
 import UserItem from "./UserItem";
 
 interface NavigationProps {}
@@ -141,11 +155,7 @@ const Navigation: FC<NavigationProps> = ({}) => {
                         isSearch
                         onClick={() => {}}
                     />
-                    <Item
-                        label="Settings"
-                        icon={Settings}
-                        onClick={() => {}}
-                    />
+                    <Item label="Settings" icon={Settings} onClick={() => {}} />
                     <Item
                         onClick={handleCreate}
                         label="New page"
@@ -154,6 +164,22 @@ const Navigation: FC<NavigationProps> = ({}) => {
                 </div>
                 <div className="mt-4">
                     <DocumentList />
+                    <Item
+                        onClick={handleCreate}
+                        icon={Plus}
+                        label="Add a page"
+                    />
+                    <Popover>
+                        <PopoverTrigger className="w-full mt-4">
+                            <Item label="Trash" icon={Trash} />
+                        </PopoverTrigger>
+                        <PopoverContent
+                            className="p-0 w-72"
+                            side={isMobile ? "bottom" : "right"}
+                        >
+                            <TrashBox />
+                        </PopoverContent>
+                    </Popover>
                 </div>
                 <div
                     onMouseDown={handleMouseDown}
---
+++ components/modals/ConfirmModal.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import React, { FC } from "react";
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from "../ui/alert-dialog";
+
+interface ConfirmModalProps {
+    children: React.ReactNode;
+    onConfirm: () => void;
+}
+
+const ConfirmModal: FC<ConfirmModalProps> = ({ children, onConfirm }) => {
+    const handleConfirm = (
+        event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+    ) => {
+        event.stopPropagation();
+        onConfirm();
+    };
+
+    return (
+        <AlertDialog>
+            <AlertDialogTrigger onClick={(e) => e.stopPropagation()}>
+                {children}
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+                <AlertDialogHeader>
+                    <AlertDialogTitle>
+                        Are you absolutely sure?
+                    </AlertDialogTitle>
+                    <AlertDialogDescription>
+                        This action cannot be undone.
+                    </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                    <AlertDialogCancel onClick={(e) => e.stopPropagation()}>
+                        Cancel
+                    </AlertDialogCancel>
+                    <AlertDialogAction onClick={handleConfirm}>
+                        Confirm
+                    </AlertDialogAction>
+                </AlertDialogFooter>
+            </AlertDialogContent>
+        </AlertDialog>
+    );
+};
+
+export default ConfirmModal;
