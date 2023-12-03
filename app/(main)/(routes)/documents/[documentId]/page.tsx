fc11b81ce8a4f77026a85f545afe0b615a814c11
Akshat <akshatsinghdelhi@gmail.com>
2023-12-03 23:08:54 +0530

    toolbar component

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,13 +1,38 @@
+"use client";
+
+import Toolbar from "@/components/Toolbar";
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { useQuery } from "convex/react";
 import { FC } from "react";
 
 interface pageProps {
     params: {
-        documentId: string;
+        documentId: Id<"documents">;
     };
 }
 
-const page: FC<pageProps> = ({ params }) => {
-    return <div>{params.documentId}</div>;
+const Page: FC<pageProps> = ({ params }) => {
+    const document = useQuery(api.documents.getById, {
+        documentId: params.documentId,
+    });
+
+    if (document === undefined) {
+        return <div>Loading...</div>;
+    }
+
+    if (document === null) {
+        return <div>Not found</div>;
+    }
+
+    return (
+        <div className="pb-40">
+            <div className="h-[35vh]" />
+            <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
+                <Toolbar initialData={document} />
+            </div>
+        </div>
+    );
 };
 
-export default page;
+export default Page;
